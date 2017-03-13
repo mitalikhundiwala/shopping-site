1ad352797798ea2422ac13b8bf625ecf0c71a04b
Mitali Patel <mitali.khundiwala@gmail.com>
2017-03-13 16:35:56 +0400

    implemented local storage caching of order data

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,4 +1,3 @@
-import { OrderItem } from '../models/order-item';
 import * as _ from 'underscore';
 import { Http, Headers } from '@angular/http';
 import { environment } from '../../environments/environment';
@@ -7,8 +6,11 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/fromPromise';
 import { BehaviorSubject } from 'rxjs/Rx';
+import * as lscache from 'lscache';
+import { OrderItem } from '../models/order-item';
 import { Order } from '../models/order';
 import { Product } from '../models/product';
+import { Category } from '../models/category';
 
 @Injectable()
 export class OrderService {
@@ -18,28 +20,89 @@ export class OrderService {
         return this._order;
     }
 
+    _parseOrderItem(orderItemData: {
+        product: {
+            id: number,
+            name: string,
+            price: number,
+            isDiscounted: boolean,
+            priceBeforeDiscount: number,
+            image: string,
+            description: string,
+            company: string,
+            categoryId: number,
+            category: Category
+        }, quantity: number
+    }): OrderItem {
+        return new OrderItem({
+            product: new Product(orderItemData.product),
+            quantity: orderItemData.quantity
+        });
+    }
+
+    _saveOrder() {
+        let order: Order = this._order.getValue();
+        let orderItems: OrderItem[] = order.orderItems.getValue();
+        let orderData: any = _.extend({
+            orderItems: _.map(orderItems, (currentOrderItem: OrderItem) => {
+                return {
+                    product: {
+                        id: currentOrderItem.product.id,
+                        name: currentOrderItem.product.name,
+                        price: currentOrderItem.product.price,
+                        isDiscounted: currentOrderItem.product.isDiscounted,
+                        priceBeforeDiscount: currentOrderItem.product.priceBeforeDiscount,
+                        image: currentOrderItem.product.image,
+                        description: currentOrderItem.product.description,
+                        company: currentOrderItem.product.company,
+                        categoryId: currentOrderItem.product.categoryId,
+                        category: null
+                    },
+                    quantity: currentOrderItem.quantity
+                };
+            })
+        });
+
+        lscache.set('order', JSON.stringify(orderData));
+    }
+
     constructor(
         private _http: Http
     ) {
         this._headers.append('Content-Type', 'application/json');
-        this._order.next(new Order({
-            orderItems: []
-        }));
+        let orderData: any = lscache.get('order') ? JSON.parse(lscache.get('order')) : null;
+        let order: Order = null;
+        if (orderData) {
+            order = new Order({
+                orderItems: _.map(orderData.orderItems, this._parseOrderItem)
+            });
+        } else {
+            order = new Order({
+                orderItems: []
+            });
+        }
+        this._order.next(order);
     }
 
     addProductToOrder(product: Product, quantity: number): OrderItem {
         let order: Order = this._order.getValue();
-        return order.addProductToOrder(product, quantity);
+        let modifiedOrderItem: OrderItem = order.addProductToOrder(product, quantity);
+        this._saveOrder();
+        return modifiedOrderItem;
     }
 
     removeProductFromOrder(product: Product): OrderItem {
         let order: Order = this._order.getValue();
-        return order.removeProductFromOrder(product);
+        let modifiedOrderItem: OrderItem = order.removeProductFromOrder(product);
+        this._saveOrder();
+        return modifiedOrderItem;
     }
 
     updateProductQuantityFromOrder(product: Product, quantity: number): OrderItem {
         let order: Order = this._order.getValue();
-        return order.updateProductQuantityFromOrder(product, quantity);
+        let modifiedOrderItem: OrderItem = order.updateProductQuantityFromOrder(product, quantity);
+        this._saveOrder();
+        return modifiedOrderItem;
     }
 
 
